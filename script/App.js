af2d408f26e5a3677cf3af0c54983fd225a888b5
Cp <yoshyparrott@gmail.com>
2015-12-02 15:57:33 +0100

    fixed it

--- script/App.js
+++ script/App.js
@@ -1,1 +1,18 @@
-document.write("HELLO WORLD");
+function validateForm() {
+    soortTafel = document.forms["myForm"]["fname"].value;
+    soortTafelNummer = parseInt(soortTafel);
+    switch (soortTafelNummer){
+      case 1:
+        tafelOefenen = 1;
+        document.write("11n");
+        break;
+      case 2:
+        tafelOefenen = 2;
+        break;
+      case 3:
+        tafelOefenen = 3;
+        break;
+      default:
+      document.write("WOLO");
+    }
+}
