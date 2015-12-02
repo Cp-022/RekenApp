e585410fd46d7c809be7f13d013d56d5afae4689
Cp <yoshyparrott@gmail.com>
2015-12-02 16:57:59 +0100

    last changes

--- script/App.js
+++ script/App.js
@@ -9,14 +9,16 @@ function exNumber(soortTafel){
     userInput = parseInt(prompt("wat is " + soortTafel + " keer " + x));
     if (userInput === answer){
       alert("yay");
+      document.getElementById("blackguy").style.property="background-image:url('../img/blackguyhey.png')";
       score = score + 1;
     }
     else{
       alert("FOUT");
+      document.getElementById("blackguy").style.property="background-image:url('../img/blackguy.png')";
     }
   }
   if ( x = 13){
-    document.write("Goedzo je hebt " + score + " goed!");  
+    document.write("Goedzo je hebt " + score + " goed!");
   }
 }
 exNumber(soortTafel);
