b1070dc546908fdb7a802ee5c4c439807daf13a3
Cp <yoshyparrott@gmail.com>
2015-12-02 16:03:09 +0100

    fixed the breaks

--- script/App.js
+++ script/App.js
@@ -12,6 +12,36 @@ function validateForm() {
       case 3:
         tafelOefenen = 3;
         break;
+      case 4:
+        tafelOefenen = 4;
+        break;
+      case 5:
+        tafelOefenen = 5;
+        break;
+      case 6:
+        tafelOefenen = 6;
+        break;
+      case 7:
+        tafelOefenen = 7;
+        break;
+      case 8:
+        tafelOefenen = 8;
+        break;
+      case 9:
+        tafelOefenen = 9;
+        break;
+      case 10:
+        tafelOefenen = 10;
+        break;
+      case 11:
+        tafelOefenen = 11;
+        break;
+      case 12:
+        tafelOefenen = 12;
+        break;
+      case 13:
+        tafelOefenen = 13;
+        break;
       default:
       document.write("WOLO");
     }
