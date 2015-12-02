6da5eae9e618ba12591ed53e9ed8a686929bdcd5
Cp <yoshyparrott@gmail.com>
2015-12-02 16:51:16 +0100

    fixed

--- script/App.js
+++ script/App.js
@@ -1,16 +1,22 @@
 soortTafel = parseInt(prompt("welke tafel wil je doen?"));
 x = 0;
 maxTafel = 13;
+score = 1;
+
 function exNumber(soortTafel){
   for (x = 0; x < maxTafel; x++){
     answer = soortTafel * x;
-    userInput = parseInt(prompt("wat is" + soortTafel + "keer" + x));
+    userInput = parseInt(prompt("wat is " + soortTafel + " keer " + x));
     if (userInput === answer){
       alert("yay");
+      score = score + 1;
     }
     else{
       alert("FOUT");
     }
+  }
+  if ( x = 13){
+    document.write("Goedzo je hebt " + score + " goed!");  
   }
 }
 exNumber(soortTafel);
