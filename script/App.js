e954282d9d4f1a6a9458e476d5f2e8c108e5cd48
Cp <yoshyparrott@gmail.com>
2015-12-06 20:36:57 +0100

    last update

--- script/App.js
+++ script/App.js
@@ -1,24 +1,36 @@
 function Alles(){
-soortTafel = parseInt(prompt("welke tafel wil je doen?"));
-x = 0;
-maxTafel = 13;
-score = 1;
+  /*IMPORTING STUFF*/
+  x = 0;
+  maxTafelAllowed = 12;
+  maxTafel = 13;
+  score = 1;
+  /*DONE IMPORTING */
 
-function exNumber(soortTafel){
-  for (x = 0; x < maxTafel; x++){
-    answer = soortTafel * x;
-    userInput = parseInt(prompt("wat is " + soortTafel + " keer " + x));
-    if (userInput === answer){
-      alert("yay");
-      score = score + 1;
+  soortTafel = parseInt(prompt("welke tafel wil je doen?"));
+  if (soortTafel > maxTafelAllowed){
+    alert("Geef een tafel van 0 tot 12 op");
+    Alles();
+  }
+  else if(isNaN(soortTafel)){
+    alert("Geef een tafel van 0 tot 12 op");
+    Alles();
+  }
+
+  function exNumber(soortTafel){
+    for (x = 0; x < maxTafel; x++){
+      answer = soortTafel * x;
+      userInput = parseInt(prompt("wat is " + soortTafel + " keer " + x));
+      if (userInput === answer){
+        alert("Goedzo");
+        score = score + 1;
+      }
+      else{
+        alert("FOUT");
+      }
     }
-    else{
-      alert("FOUT");
+    if ( x = 13){
+      document.write("Goedzo je hebt " + score + " goed!");
     }
   }
-  if ( x = 13){
-    document.write("Goedzo je hebt " + score + " goed!");
-  }
-}
-exNumber(soortTafel);
+  exNumber(soortTafel);
 }
