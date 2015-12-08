670de8cecec73f70f3a91b596061d613f96cc480
Cp <yoshyparrott@gmail.com>
2015-12-08 11:13:09 +0100

    huge overhaul

--- script/App.js
+++ script/App.js
@@ -1,3 +1,14 @@
+function Submit(){
+  if (antwoord === answer){
+    score = score + 1;
+  }
+  else{
+    /*EMPTY*/
+  }
+  if (score <5){
+    alert("jammer");
+  }
+}
 function Alles(){
   /*IMPORTING STUFF*/
   x = 0;
@@ -19,17 +30,9 @@ function Alles(){
   function exNumber(soortTafel){
     for (x = 0; x < maxTafel; x++){
       answer = soortTafel * x;
-      userInput = parseInt(prompt("wat is " + soortTafel + " keer " + x));
-      if (userInput === answer){
-        alert("Goedzo");
-        score = score + 1;
-      }
-      else{
-        alert("FOUT");
-      }
-    }
-    if ( x = 13){
-      document.write("Goedzo je hebt " + score + " goed!");
+      document.getElementById("vragenlijst").innerHTML +="<span id=vraag"+x+">";
+      document.getElementById("vraag"+x).innerHTML = "<p><span>wat is " + soortTafel + " keer " + x + "</span>" + "<input type=text id='antwoord'+"+x + "> </input></p>";
+      antwoord = document.getElementById('antwoord'+x);
     }
   }
   exNumber(soortTafel);
