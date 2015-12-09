44f95b8eaf43679a0142b43399f5076f013d5306
Cp <yoshyparrott@gmail.com>
2015-12-09 08:54:31 +0100

    got it working

--- script/App.js
+++ script/App.js
@@ -1,9 +1,22 @@
 function Submit(){
-
+  for (x = 0; x < maxTafel; x++){
+    if(parseInt(document.getElementById('antwoord'+x).value) === antwoorden[x]){
+      score++;
+      alert("OMG GOED");
+    }
+    else{
+      score--;
+      alert("BADJAS");
+    }
+  }
+  if(x === 13){
+    document.write(score);
+  }
 }
 function Alles(){
   /*IMPORTING STUFF*/
   x = 0;
+  antwoorden = new Array();
   maxTafelAllowed = 12;
   maxTafel = 13;
   score = 1;
@@ -21,8 +34,10 @@ function Alles(){
 
   function exNumber(soortTafel){
     for (x = 0; x < maxTafel; x++){
+      antwoorden[x] = soortTafel * x;
+
       document.getElementById("vragenlijst").innerHTML +="<span id=vraag"+x+">";
-      document.getElementById("vraag"+x).innerHTML = "<p><span>wat is " + soortTafel + " keer " + x + "</span> " + "<input type=text id='antwoord'+"+x + "> </input></p>";
+      document.getElementById("vraag"+x).innerHTML = "<p><span>wat is " + soortTafel + " keer " + x + "</span> " + "<input type=text id='antwoord"+x + "'> </input></p>";
     }
   }
   exNumber(soortTafel);
